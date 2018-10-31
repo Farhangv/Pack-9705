9d19e4c1383b9f2fbc08718dc3ed7e545ec95965
Farhang Vafa <farhangv@ymail.com>
2018-10-31 13:42:25 +0330

    970809

---
+++ 05.ASPNET MVC/Session32-970809/MVCDemo/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+}
