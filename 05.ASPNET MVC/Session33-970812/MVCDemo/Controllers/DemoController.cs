c054e22b9f0ac14c1f413e7a9b74d5c0a9833ea8
Farhang Vafa <farhangv@ymail.com>
2018-11-03 13:28:49 +0330

    970812

---
+++ 05.ASPNET MVC/Session33-970812/MVCDemo/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Razor()
+        {
+            return View();
+        }
+
+        public ActionResult ViewDataDemo()
+        {
+            var people = new List<Person>()
+            {
+                new Person() { Name = "رضا", Family = "رضایی", Age = 30 },
+                new Person() { Name = "میترا", Family = "میترایی", Age = 20 },
+                new Person() { Name = "پیام", Family = "پیامکی", Age = 25 },
+                new Person() { Name = "مریم", Family = "مریمی", Age = 28 }
+            };
+
+            ViewData["Users"] = people;
+            ViewData["Message"] = "شما میتواند کاربران موجود را در لیست زیر ببینید";
+
+            return View();
+        }
+    }
+}
