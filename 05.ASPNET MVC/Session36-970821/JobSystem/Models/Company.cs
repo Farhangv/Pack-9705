412f5fd8b7183861ade0fd667b24d2c8f255e861
Farhang Vafa <farhangv@ymail.com>
2018-11-13 00:05:20 +0330

    970821

---
+++ 05.ASPNET MVC/Session36-970821/JobSystem/Models/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace JobSystem.Models
+{
+    [Table("Company", Schema = "Jobs")]//Data Annotation
+    public class Company
+    {
+        //[Key]//PK
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]//Identity
+        public int Id { get; set; }
+        [MaxLength(100), Required]
+        public string Name { get; set; }
+        [Required, RegularExpression(@"^09\d{9}$")]
+        [Column("Phone", TypeName = "varchar")]
+        [Index(IsUnique = true)]
+        public string CellPhone { get; set; }
+        [StringLength(100, MinimumLength = 3)]
+        public string Email { get; set; }
+        [MaxLength(100)]
+        public string Website { get; set; }
+        public string LogoFilePath { get; set; }
+        [MaxLength(400)]
+        public string Description { get; set; }
+        public virtual List<Job> Jobs { get; set; }//Lazy Loading
+
+    }
+}
---
+++ 05.ASPNET MVC/Session36-970821/JobSystem/Models/Resume.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace JobSystem.Models
+{
+    [Table("Resume", Schema = "Jobs")]
+    public class Resume
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Title { get; set; }
+        public byte[] ResumeFile { get; set; }
+        [MaxLength(20)]
+        public string MimeType { get; set; }
+        [MaxLength(100)]
+        public string OriginalFileName { get; set; }
+        public int FileSize { get; set; }
+        public virtual List<Job> Jobs { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
