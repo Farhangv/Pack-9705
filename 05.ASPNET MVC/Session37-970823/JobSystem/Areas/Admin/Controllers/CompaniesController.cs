662b2b8a2a7af4a44ebf4e370a697f54a5eecb4a
Farhang Vafa <farhangv@ymail.com>
2018-11-15 02:19:38 +0330

    970823

---
+++ 05.ASPNET MVC/Session37-970823/JobSystem/Areas/Admin/Controllers/CompaniesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JobSystem.Models;
+
+namespace JobSystem.Areas.Admin.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private JobDbContext db = new JobDbContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Companies.ToList());
+        }
+
+        // GET: Admin/Companies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult(400);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                //return HttpNotFound();
+                return new HttpStatusCodeResult(404);
+            }
+            return View(company);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Companies.Add(company);
+                db.SaveChanges();
+                TempData["Message"] = "شرکت با موفقیت افزوده شد";
+                TempData["MessageClass"] = "success";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(company);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(company).State = EntityState.Modified;
+                //Company db_company = db.Companies.Find(company.Id);
+                //db_company.Name = company.Name;
+                //db_company.CellPhone = company.CellPhone;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(company);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Admin/Companies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Company company = db.Companies.Find(id);
+            db.Companies.Remove(company);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
